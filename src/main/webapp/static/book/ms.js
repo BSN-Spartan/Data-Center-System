b5c66c6f687c7e79da49bda4fe1ab3379d73be2e
sytang1991 <67669722+sytang1991@users.noreply.github.com>
2022-09-05 17:49:04 +0800

    Update quick-start

---
+++ src/main/webapp/static/book/ms.js
@@ -0,0 +1,3 @@
+gitbook build
+cp  _book\img\apple-touch-icon-precomposed-152.png _book\gitbook\images\apple-touch-icon-precomposed-152.png
+cp  _book\img\favicon.ico _book\gitbook\images\favicon.ico
